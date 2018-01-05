1eea5b1056ec931b59d87a5ff7bb772b2e0bf094
Javier Llamas <javier.llamasramirez@gmail.com>
2018-01-04 23:12:06 -0600

    script incluye instalacion de requerimientos

--- scripts/install_dev_env.py
+++ scripts/install_dev_env.py
@@ -2,25 +2,17 @@ import subprocess, shlex
 from pathlib import Path
 import os
 
-
-#subprocess.run(['/usr/local/bin/virtualenvwrapper.sh && mkvirtualenv -h'], executable='bash')
-#subprocess.Popen(['bash', '-c', '. /usr/local/bin/virtualenvwrapper.sh; mkvirtualenv'])
-# library_path = '/usr/local/bin/virtualenvwrapper.sh'
-# function_name = 'mkvirtualenv'
-# params = '-h'
-# params = shlex.split('"source %s; %s %s"' % (library_path, function_name, params))
-# cmdline = ['bash', '-c'] + params
-# subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 home = home = str(Path.home())
 VIRTUAL_ENV_NAME = 'construbot'
-POSTACTIVE_LOCATION = home + '/.virtualenvs/{}/bin/postactivate'.format(VIRTUAL_ENV_NAME)
+VIRTUAL_ENV_FOLDER = home + '/.virtualenvs/{}/'.format(VIRTUAL_ENV_NAME)
+POSTACTIVE_LOCATION = VIRTUAL_ENV_FOLDER + 'bin/postactivate'
 SCRIPT_LOCATION = '/usr/local/bin/virtualenvwrapper.sh'
-ACTIVATE = home + '/.virtualenvs/{}/bin/activate'.format(VIRTUAL_ENV_NAME)
+ACTIVATE = home + VIRTUAL_ENV_FOLDER + 'bin/activate'
 cwd = os.getcwd()
+
 def run_bash_function(library_path, function_name, params):
     params = shlex.split('"source %s; %s %s"' % (library_path, function_name, params))
     cmdline = ['bash', '-c'] + params
-    print(cmdline)
     process = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     stdout, stderr = process.communicate()
     if process.returncode != 0:
@@ -35,19 +27,14 @@ def configure_virtual_env(postactive_location=POSTACTIVE_LOCATION, name=VIRTUAL_
     with open(postactive_location, 'w', newline='\n') as file:
         file.write('{0}_root={1}\ncd ${0}_root\nPATH=${0}_root/bin:$PATH'.format(name, cwd))
 
-# def workon_virtualenv(script=ACTIVATE, env_name=VIRTUAL_ENV_NAME):
-#     #run_bash_function(script, 'workon', env_name)
-#     #command = '. ' + script + ';' + ' workon ' + env_name
-#     subprocess.Popen(['bash', '-c', script], shell=True, executable='bash')
-#     #params = shlex.split('"source %s; %s %s"' % (script, 'workon', env_name))
-#     #cmdline = ['bash', '-c'] + params
-#     #subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-
+def update_virtual_env(venv_folder=VIRTUAL_ENV_FOLDER):
+    pip_location = venv_folder + 'bin/pip'
+    subprocess.run([pip_location, 'install', '-r', 'requirements.txt'])
 
 def main(venv_name=VIRTUAL_ENV_NAME, venv_wrapper=SCRIPT_LOCATION, postactive_location=POSTACTIVE_LOCATION):
     make_virtual_env()
     configure_virtual_env()
-    #workon_virtualenv()
+    update_virtual_env()
 
 
 if __name__ == '__main__':
