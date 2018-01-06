e392c6877fb0e88a6343990e92b55572f4970fea
Norma Placencia <norma.placencia@insecami.com.mx>
2018-01-05 19:11:15 -0600

    ya se crea el ambiente virtual en windows

--- scripts/install_dev_env.py
+++ scripts/install_dev_env.py
@@ -59,10 +59,12 @@ def make_virtual_env(script_location=SCRIPT_LOCATION, name=VIRTUAL_ENV_NAME):
         # raise FileNotFoundError
         #
         # script_location = VIRTUAL_ENV_FOLDER + 'Scripts/mkvirtualenv.bat'
-        proceso = subprocess.run(script_location, shell=True, stdout=subprocess.PIPE)
+        command = script_location + ' construbot'
+        proceso = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #import pdb; pdb.set_trace()
         if proceso.returncode != 0:
-            raise RuntimeError("'%s' failed, error code: '%s', stdout: '%s', stderr: '%s'" % (
-                ' '.join(script_location), proceso.returncode, stdout.rstrip(), stderr.rstrip()))
+            raise RuntimeError("'%s' failed, error code: '%s'" % (
+                ' '.join(script_location), proceso.returncode))
 
 def configure_virtual_env(postactive_location=POSTACTIVE_LOCATION, name=VIRTUAL_ENV_NAME):
     if get_platform() == 'unix':
