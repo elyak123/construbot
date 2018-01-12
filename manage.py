9badbbb7659ee4528e4032b9d039f98fc88fab1d
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-01-12 14:37:20 -0600

    cambio para poner cookiecutter

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "construbot.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -11,7 +12,7 @@ if __name__ == "__main__":
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            import django
+            import django  # noqa
         except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
@@ -19,4 +20,10 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+
+    # This allows easy placement of apps within the interior
+    # construbot directory.
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, 'construbot'))
+
     execute_from_command_line(sys.argv)
