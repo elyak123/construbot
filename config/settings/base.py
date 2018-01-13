b62b03cfe1265915cc84a67dabfaf35d42cc4c96
Javier Llamas <javier.llamasramirez@gmail.com>
2018-01-12 18:25:00 -0600

    cambio de layout con cookiecutter

--- config/settings/base.py
+++ config/settings/base.py
@@ -122,7 +122,7 @@ DATABASES['default']['ATOMIC_REQUESTS'] = True
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'UTC-6'
+TIME_ZONE = 'America/Mexico_City'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = 'en-us'
--- construbot/users/urls.py
+++ construbot/users/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'construbot.users'
+
 urlpatterns = [
     url(
         regex=r'^$',
