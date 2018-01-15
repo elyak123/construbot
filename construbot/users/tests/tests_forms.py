ba79a8ebb00602717f66987818adccaf1f00ed15
Javier Llamas <javier.llamasramirez@gmail.com>
2018-01-14 20:03:09 -0600

    falta prueba de save

---
+++ construbot/users/tests/tests_forms.py
@@ -0,0 +1,23 @@
+from construbot.users.forms import UserForm
+from django.test import TestCase
+
+
+class UserFormTest(TestCase):
+    def test_userform_setting_is_correct(self):
+        from django.conf import settings
+        try:
+            settings.ACCOUNT_SIGNUP_FORM_CLASS
+        except AttributeError:
+            self.fail('Django settings doesnt declare ACCOUNT_SIGNUP_FORM_CLASS')
+        self.assertEqual(settings.ACCOUNT_SIGNUP_FORM_CLASS, 'construbot.users.forms.UserForm')
+
+    def test_userform_post(self):
+        form = UserForm(data={
+            'email': 'bla@email.com',
+            'username': 'some_name',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'password1': 'Secret 19384',
+            'password2': 'Secret 19384',
+            })
+        self.assertTrue(form.is_valid())
