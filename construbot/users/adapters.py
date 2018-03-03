79d1630464bbf170a6ae4e40b1e1c66a9deb6516
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-03-03 15:16:15 -0600

    cambios

--- construbot/users/adapters.py
+++ construbot/users/adapters.py
@@ -1,9 +1,15 @@
+from .models import Customer
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
 class AccountAdapter(DefaultAccountAdapter):
+    def save_user(self, request, user, form, commit=True):
+        new_customer = Customer.objects.create(customer_name='yo')
+        user.customer = new_customer
+        return super(AccountAdapter, self).save_user(request, user, form, commit=True)
+
     def is_open_for_signup(self, request):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
 
--- construbot/users/forms.py
+++ construbot/users/forms.py
@@ -6,14 +6,13 @@ from django.contrib.auth.forms import UserCreationForm
 class UserForm(UserCreationForm):
 
     def signup(self, request, user):
-        # crear aqui el customer
         pass
 
     class Meta:
         model = User
         exclude = [
             'password',
-            #'customer',
+            'customer',
             'company',
             'last_login',
             'is_superuser',
