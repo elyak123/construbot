be4371db34b7ef6bb61f794a66dacdf61f9e92a5
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-03-27 19:41:03 -0600

    prueba ContextManager con menu_specific

--- construbot/core/tests.py
+++ construbot/core/tests.py
@@ -23,3 +23,18 @@ class ContextTests(utils.BaseTestCase):
                     obj['title'].lower() in ContextManager.user_groups,
                     '%s is not in user_groups' % obj['title']
                 )
+
+    def test_context_includes_menu_specific_right_place(self):
+        with mock.patch('django.urls.reverse') as reverse_mock:
+            reverse_mock.return_value = '/fake/url/'
+            reverse_mock.side_effect = 'fake/url/'
+            view = ContextManager()
+            view.user_groups = [
+                'home', 'documentos', 'pendientes', 'proyectos'
+            ]
+            menu_specific_obj = [{'title': 'fake'}, {'title': 'foo'}]
+            view.menu_specific = menu_specific_obj
+            view.app_label_name = 'Documentos'
+            menu = view.get_menu()
+            self.assertIn(menu_specific_obj[0], menu)
+            self.assertEqual(menu.index(menu_specific_obj[0]), 2)
