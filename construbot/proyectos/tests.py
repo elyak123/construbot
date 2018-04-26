6142df07734a2b5f265ea9d035f9e219f8ae5b87
Carlos Daniel <carlosintegra7@gmail.com>
2018-04-26 12:43:28 -0500

    correccion de detalles

--- construbot/proyectos/tests.py
+++ construbot/proyectos/tests.py
@@ -8,7 +8,7 @@ from .views import (ContratoListView, ClienteListView, SitioListView, Destinatar
                     SitioCreationView, DestinatarioCreationView, ContratoEditView, ClienteEditView,
                     SitioEditView, DestinatarioEditView, CatalogoConceptosInlineFormView, SitioAutocomplete,
                     ClienteAutocomplete, UnitAutocomplete)
-from .forms import (ContratoForm, ClienteForm, SitioForm, DestinatarioForm, ContractConceptInlineForm)
+from .forms import (ContratoForm, ClienteForm, SitioForm, DestinatarioForm)
 from .models import Destinatario, Sitio, Cliente, Contrato
 from construbot.users.models import User, Company, Customer
 from django.core.management import call_command
