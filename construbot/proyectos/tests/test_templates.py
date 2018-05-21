ffda6573484086dc7dea25a9604a2805d88adb08
Carlos <carlosdany58@hotmail.com>
2018-05-21 12:21:49 -0500

    adicion de tests para códigos 200

--- construbot/proyectos/tests/test_templates.py
+++ construbot/proyectos/tests/test_templates.py
@@ -1,9 +1,10 @@
 from construbot.users.tests import factories as user_factories
 from django.core.urlresolvers import reverse, resolve
+from construbot.proyectos.models import Estimate
 from . import factories
 from test_plus.test import TestCase
 
-class TestProyectsURLs(TestCase):
+class TestProyectsURLsCorrectTemplates(TestCase):
     """Test URL patterns for users app."""
 
     def setUp(self):
@@ -78,10 +79,21 @@ class TestProyectsURLs(TestCase):
         response = self.client.get(reverse('proyectos:destinatario_detail', kwargs={'pk': destinatario.pk}))
         self.assertTemplateUsed(response, 'proyectos/destinatario_detail.html')
 
-    # def test_estimate_detail_uses_correct_template(self):
-    #     self.client.login(username=self.user.username, password='password')
-    #     response = self.client.get(reverse('proyectos:estimate_detail', kwargs={'pk': 1}))
-    #     self.assertTemplateUsed(response, 'proyectos/')
+    def test_estimate_detail_uses_correct_template(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        estimate = Estimate.objects.create(
+            project=contrato_factory,
+            consecutive=1,
+            draft_by=self.user,
+            supervised_by=self.user,
+            start_date='1999-08-15',
+            finish_date='1999-08-15'
+        )
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:estimate_detail', kwargs={'pk': estimate.pk}))
+        self.assertTemplateUsed(response, 'proyectos/estimate_detail.html')
 
     def test_nuevo_contrato_uses_correct_template(self):
         self.client.login(username=self.user.username, password='password')
@@ -138,10 +150,21 @@ class TestProyectsURLs(TestCase):
         response = self.client.get(reverse('proyectos:editar_destinatario', kwargs={'pk': destinatario.pk}))
         self.assertTemplateUsed(response, 'proyectos/creation_form.html')
 
-    # def test_editar_estimacion_uses_correct_template(self):
-    #     self.client.login(username=self.user.username, password='password')
-    #     response = self.client.get(reverse('proyectos:editar_estimacion', kwargs={'pk': 1}))
-    #     self.assertTemplateUsed(response, 'proyectos/creation_form.html')
+    def test_editar_estimacion_uses_correct_template(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        estimate = Estimate.objects.create(
+            project=contrato_factory,
+            consecutive=1,
+            draft_by=self.user,
+            supervised_by=self.user,
+            start_date='1999-08-15',
+            finish_date='1999-08-15'
+        )
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:editar_estimacion', kwargs={'pk': estimate.pk}))
+        self.assertTemplateUsed(response, 'proyectos/estimate_form.html')
 
     def test_delete_models_uses_correct_template(self):
         contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
@@ -150,3 +173,174 @@ class TestProyectsURLs(TestCase):
         self.client.login(username=self.user.username, password='password')
         response = self.client.get(reverse('proyectos:eliminar', kwargs={'model': 'Contrato', 'pk': contrato_factory.pk}))
         self.assertTemplateUsed(response, 'core/delete.html')
+
+class TestProyectsURLsCorrectStatusCode(TestCase):
+    
+    def setUp(self):
+        self.user_factory = user_factories.UserFactory
+        self.user = self.make_user()
+        company_test = user_factories.CompanyFactory(customer=self.user.customer)
+        administrador = user_factories.GroupFactory(name="Administrators")
+        proyectos = user_factories.GroupFactory(name="Proyectos")
+        users = user_factories.GroupFactory(name="Users")
+        for group in administrador, proyectos, users:
+            self.user.groups.add(group)
+        self.user.company.add(company_test)
+        self.user.currently_at = company_test
+
+    def test_proyects_dashboard_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:proyect_dashboard'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contrato_list_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:listado_de_contratos'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_clientes_list_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:listado_de_clientes'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_sitios_list_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:listado_de_sitios'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_destinatarios_list_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:listado_de_destinatarios'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_catalogo_edit_has_correct_status_code(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:catalogo_conceptos', kwargs={'pk': contrato_factory.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contrato_detail_has_correct_status_code(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:contrato_detail', kwargs={'pk': contrato_factory.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_cliente_detail_has_correct_status_code(self):
+        cliente = factories.ClienteFactory(company=self.user.company.first())
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:cliente_detail', kwargs={'pk': cliente.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_sitio_detail_has_correct_status_code(self):
+        sitio = factories.SitioFactory(company=self.user.company.first())
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:sitio_detail', kwargs={'pk': sitio.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_destinatario_detail_has_correct_status_code(self):
+        destinatario_cliente = factories.ClienteFactory(company=self.user.company.first())
+        destinatario = factories.DestinatarioFactory(cliente=destinatario_cliente, company=self.user.company.first())
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:destinatario_detail', kwargs={'pk': destinatario.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_estimate_detail_has_correct_status_code(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        estimate = Estimate.objects.create(
+            project=contrato_factory,
+            consecutive=1,
+            draft_by=self.user,
+            supervised_by=self.user,
+            start_date='1999-08-15',
+            finish_date='1999-08-15'
+        )
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:estimate_detail', kwargs={'pk': estimate.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_nuevo_contrato_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:nuevo_contrato'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_nuevo_cliente_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:nuevo_cliente'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_nuevo_sitio_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:nuevo_sitio'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_nuevo_destinatario_has_correct_status_code(self):
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:nuevo_destinatario'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_nueva_estimacion_has_correct_status_code(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:nueva_estimacion', kwargs={'pk': contrato_factory.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_editar_contrato_has_correct_status_code(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:editar_contrato', kwargs={'pk': contrato_factory.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_editar_cliente_has_correct_status_code(self):
+        cliente = factories.ClienteFactory(company=self.user.company.first())
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:editar_cliente', kwargs={'pk': cliente.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_editar_sitio_has_correct_status_code(self):
+        sitio = factories.SitioFactory(company=self.user.company.first())
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:editar_sitio', kwargs={'pk': sitio.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_editar_destinatario_has_correct_status_code(self):
+        destinatario_cliente = factories.ClienteFactory(company=self.user.company.first())
+        destinatario = factories.DestinatarioFactory(cliente=destinatario_cliente, company=self.user.company.first())
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:editar_destinatario', kwargs={'pk': destinatario.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_editar_estimacion_has_correct_status_code(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        estimate = Estimate.objects.create(
+            project=contrato_factory,
+            consecutive=1,
+            draft_by=self.user,
+            supervised_by=self.user,
+            start_date='1999-08-15',
+            finish_date='1999-08-15'
+        )
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:editar_estimacion', kwargs={'pk': estimate.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_models_has_correct_status_code(self):
+        contrato_cliente = factories.ClienteFactory(company=self.user.company.first())
+        contrato_sitio = factories.SitioFactory(company=self.user.company.first())
+        contrato_factory = factories.ContratoFactory(cliente=contrato_cliente, sitio=contrato_sitio, monto=90.00)
+        self.client.login(username=self.user.username, password='password')
+        response = self.client.get(reverse('proyectos:eliminar', kwargs={'model': 'Contrato', 'pk': contrato_factory.pk}))
+        self.assertEqual(response.status_code, 200)
+
+        
