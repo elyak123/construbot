ab180ddf1bfd82febb0bdf072ba53c872868d7a7
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-06-05 18:08:31 -0500

    test para estimateform

--- construbot/proyectos/tests/test_forms.py
+++ construbot/proyectos/tests/test_forms.py
@@ -230,3 +230,36 @@ class BaseCleanFormTest(utils.BaseTestCase):
         form.cleaned_data = {'company': mock.Mock()}
         with self.assertRaises(forms.forms.ValidationError):
             form.clean()
+
+
+class EstimateFormTest(utils.BaseTestCase):
+
+    def setUp(self):
+        self.user_factory = user_factories.UserFactory
+        self.user = self.make_user()
+        self.factory = RequestFactory()
+        self.request = self.get_request(self.user)
+
+    def test_clean_estimateform_raises_different_company_client_and_contrato(self):
+        contrato = factories.ContratoFactory()
+        destinatario_2 = factories.DestinatarioFactory(
+            company=contrato.cliente.company,
+            cliente=contrato.cliente
+        )
+        destinatario = factories.DestinatarioFactory(
+            company=contrato.cliente.company,
+            #cliente=contrato.cliente
+        )
+        form_data = {
+            'project': str(contrato.pk),
+            'consecutive': '1',
+            'draft_by': [str(self.user.pk)],
+            'supervised_by': [str(self.user.pk)],
+            'start_date': '2018-03-18',
+            'finish_date': '2018-04-12',
+            'auth_by': [str(destinatario_2.pk)],
+            'auth_by_gen': [str(destinatario.pk)],
+
+        }
+        form = forms.EstimateForm(form_data)
+        self.assertFalse(form.is_valid())
