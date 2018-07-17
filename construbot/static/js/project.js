b1bb248c4927b22e2fa381d2d59a5d7a60dfd1ff
Carlos Daniel <carlosdany58@hotmail.com>
2018-07-17 15:03:40 -0500

    correccion de bug en formulario2

--- construbot/static/js/project.js
+++ construbot/static/js/project.js
@@ -1,23 +1,3 @@
-/* Project specific Javascript goes here. */
-
-/*
-Formatting hack to get around crispy-forms unfortunate hardcoding
-in helpers.FormHelper:
-
-    if template_pack == 'bootstrap4':
-        grid_colum_matcher = re.compile('\w*col-(xs|sm|md|lg|xl)-\d+\w*')
-        using_grid_layout = (grid_colum_matcher.match(self.label_class) or
-                             grid_colum_matcher.match(self.field_class))
-        if using_grid_layout:
-            items['using_grid_layout'] = True
-
-Issues with the above approach:
-
-1. Fragile: Assumes Bootstrap 4's API doesn't change (it does)
-2. Unforgiving: Doesn't allow for any variation in template design
-3. Really Unforgiving: No way to override this behavior
-4. Undocumented: No mention in the documentation, or it's too hard for me to find
-*/
 $(document).ready(function(){
 	// $('.form-group').removeClass('row');
     var menu = $(".cont_menu_lateral");
@@ -297,5 +277,17 @@ $(document).ready(function(){
         $(document).on('change', '.custom-file-input', function(){
             $(this).parent().find(".custom-file-label")[0].innerText = $(this).val().replace(/C:\\fakepath\\/i, '');
         });
+    }
+    if($("#select2-id_cliente-container").length && $("#select2-id_sitio-container").length){
+        $("#id_cliente").on("change", function (){
+            if($("#select2-id_cliente-container")[0].childNodes[0].nodeName != "SPAN"){
+                $("#select2-id_sitio-container")[0].parentNode.parentNode.parentNode.parentNode.style.display = "block";
+            } else {
+                $("#select2-id_sitio-container")[0].parentNode.parentNode.parentNode.parentNode.style.display = "none";
+            }
+        });
+        if($("#select2-id_cliente-container")[0].childNodes[0].nodeName == "SPAN"){
+            $("#select2-id_sitio-container")[0].parentNode.parentNode.parentNode.parentNode.style.display = "none";
+        }
     }
 });
