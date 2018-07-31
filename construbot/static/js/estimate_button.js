b3c5f6427a577572cf571e0ea19bb2990f8ae99c
Carlos Daniel <carlosdany58@hotmail.com>
2018-07-31 10:02:53 -0500

    cálculo de cantidad estimada con JS

---
+++ construbot/static/js/estimate_button.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  let button = $(".calcular_cantidad");
+  button.on("click", function n(ev){
+    calcular(ev);
+  });
+  function calcular(ev){
+    ev.preventDefault();
+    let source = ev.target.parentElement.parentElement;
+    let field = source.children[1].children[0].children[1];
+    let values = source.parentElement;
+    let v1 = parseFloat(values.children[0].children[0].children[1].value);
+    let v2 = parseFloat(values.children[1].children[0].children[1].value);
+    let v3 = parseFloat(values.children[2].children[0].children[1].value);
+    field.value = (v1*v2*v3).toFixed(2);
+  }
+
+});
