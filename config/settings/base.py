5daf99c0d036c315ae915c97a7db8e428cfa46e4
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-08-06 17:13:26 -0500

    JWT implementado

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,6 +91,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# REST_FRAMEWORK
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
 # MIGRATIONS CONFIGURATION
 # ------------------------------------------------------------------------------
 MIGRATION_MODULES = {
--- construbot/api/urls.py
+++ construbot/api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from . import views
 
@@ -6,4 +7,7 @@ app_name = 'construbot.api'
 
 urlpatterns = [
     url(regex='customer/list/', view=views.customer_list, name='customerlist'),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
