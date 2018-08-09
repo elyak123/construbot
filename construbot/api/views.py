6c7aa27b18e7199e45de9508131b69a1668eb39a
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-08-09 18:21:36 -0500

    se incorpora permisos de staff

--- construbot/api/views.py
+++ construbot/api/views.py
@@ -1,17 +1,20 @@
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 from construbot.users.models import Customer
 from construbot.api.serializers import CustomerSerializer
 
 
 class CustomerList(generics.ListCreateAPIView):
+    permission_classes = (IsAdminUser,)
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
 
 
 class UserRetrive(generics.RetrieveAPIView):
+    permission_classes = (IsAdminUser,)
     lookup_field = 'email'
 
     def get_queryset(self):
-        user_model = settings.AUTH_USER_MODEL
-        return user_model.objects
+        User = get_user_model()
+        return User.objects
