83dedc064befc58295c9152d095c94e501be0d86
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-08-12 08:54:48 -0500

    modificacion para enviar el token en lugar del pwd

--- construbot/api/views.py
+++ construbot/api/views.py
@@ -1,15 +1,16 @@
+import time
+import random
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.conf import settings
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from construbot.users.models import Customer
-from construbot.api.serializers import CustomerSerializer, UserSerializer
+from rest_framework_jwt.settings import api_settings
 from construbot.users.models import User, Company, Customer
-from django.contrib.auth.models import Group
-from django.conf import settings
-import time
-import random
+from construbot.api.serializers import CustomerSerializer, UserSerializer
+
 
 class CustomerList(generics.ListCreateAPIView):
     permission_classes = (IsAdminUser,)
@@ -34,14 +35,15 @@ def email_uniqueness(request):
             user = None
         return Response({'unique': user is None})
 
+
 @api_view(['POST'])
 def create_customer_user_and_company(request):
     name = settings.UUID+'+'+str(time.time())
     pwd = str(time.time()+random.random()*2000)
     if request.method == 'POST':
-        group_a = Group.objects.get(name='Administrators')
-        group_p = Group.objects.get(name='Proyectos')
-        group_u = Group.objects.get(name='Users')
+        group_a, a_created = Group.objects.get_or_create(name='Administrators')
+        group_p, b_created = Group.objects.get_or_create(name='Proyectos')
+        group_u, c_created = Group.objects.get_or_create(name='Users')
         customer = Customer.objects.create(customer_name=request.data['customer'])
         company = Company.objects.create(customer=customer, company_name=name)
         user = User.objects.create_user(
@@ -52,5 +54,9 @@ def create_customer_user_and_company(request):
         )
         user.company = [company.id]
         user.groups.add(*[group_a, group_p, group_u])
-        import pdb; pdb.set_trace()
-        return Response({'email': user.email, 'res': pwd})
+        # Creamos un token y lo enviamos en lugar del password
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        return Response({'email': user.email, 'token': token})
