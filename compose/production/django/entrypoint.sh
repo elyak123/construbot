c49da0c635d42c4c7e4f1e2bfcfdb44aecfc980a
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-09-14 14:34:38 -0500

    se agrega host para variable de entorno de la base de datos

--- compose/production/django/entrypoint.sh
+++ compose/production/django/entrypoint.sh
@@ -20,8 +20,11 @@ if [ -z "$POSTGRES_USER" ]; then
     export POSTGRES_USER=postgres
 fi
 
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
+if [ -z "$POSTGRES_HOST" ]; then
+        export POSTGRES_HOST=postgres
+fi
 
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/$POSTGRES_USER
 export CELERY_BROKER_URL=$REDIS_URL/0
 
 
@@ -30,7 +33,7 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="$POSTGRES_HOST")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
