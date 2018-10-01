04644d5a4c8f466e413b22b3270447ab07c52a01
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-09-30 19:45:06 -0500

    correccion de errores en API

--- construbot/api/tests/test_views.py
+++ construbot/api/tests/test_views.py
@@ -4,6 +4,7 @@ from construbot.api.views import email_uniqueness
 from django.test import RequestFactory, tag
 from django.urls import reverse
 
+
 class BaseCoreTestCase(utils.BaseTestCase):
 
     def setUp(self):
@@ -11,18 +12,32 @@ class BaseCoreTestCase(utils.BaseTestCase):
         self.user = self.make_user()
         self.factory = RequestFactory()
 
+
 class TestApiViews(BaseCoreTestCase):
-    
+
     def test_email_uniqueness(self):
         self.client.login(username=self.user.username, password='password')
         response = self.client.post(reverse('api:get_user'), data={'email': self.user.email})
         self.assertFalse(response.data['unique'])
-    
+    @tag('current')
+    def test_create_customer_user_and_company_raises_not_valid_email(self):
+        self.client.login(username=self.user.username, password='password')
+        email_test = 'Soy correo mal formado'
+        response = self.client.post(
+            reverse('api:creation'), data={'customer': 'nuevo_customer', 'email': email_test}
+        )
+        self.assertFalse(response.json()['success'])
+
+    @tag('current')
     def test_create_customer_user_and_company(self):
         self.client.login(username=self.user.username, password='password')
-        email_test = 'a@a.com'
-        response = self.client.post(reverse('api:creation'), data={'customer': 'nuevo_customer', 'email': email_test})
-        self.assertEqual(email_test, User.objects.get(id=response.data['id']).email)
+        email_test = 'me@gmail.com'
+        response = self.client.post(
+            reverse('api:creation'), data={'customer': 'nuevo_customer', 'email': email_test}
+        )
+        print(response.json())
+        self.assertTrue(response.json()['success'], msg=response.json())
+        self.assertEqual(email_test, User.objects.get(id=response.json()['id']).email)
         self.assertFalse(response.data['usable'])
 
     def test_change_user_password(self):
