ede275cf7b20d2868db11907785be1b68dffc93d
Javier Llamas Ramirez <elyak.123@gmail.com>
2018-10-13 14:31:57 -0500

    se agrega setup.py para empacar construbot

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-construbot',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU Affero General Public License v3',
+    description='A simple Django app to conduct Web-based polls.',
+    long_description=README,
+    url='https://www.construbot.org/',
+    author='Javier Llamas Ramirez',
+    author_email='elyak.123@gmail.com',
+    python_requires='>=3.6.2',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
