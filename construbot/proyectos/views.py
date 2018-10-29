2fb7ae7fc16f24819349cf66850b5307b52caf96
Carlos Daniel <carlosdany58@hotmail.com>
2018-10-29 11:23:38 -0600

    solucion PDF, renderiza cualquier concepto e imagenes

--- construbot/proyectos/views.py
+++ construbot/proyectos/views.py
@@ -211,6 +211,7 @@ class EstimateDetailView(DynamicDetail):
     def get_context_data(self, **kwargs):
         context = super(EstimateDetailView, self).get_context_data(**kwargs)
         context["conceptos"] = self.object.anotaciones_conceptos()
+        context["cantidad_de_conceptos"] = len(context["conceptos"])
         return context
 
 
