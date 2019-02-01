a1bb04772ce6f00ca46164fce5e93478f552298e
Carlos Daniel <carlosdany58@hotmail.com>
2019-02-01 12:25:59 -0600

    se añaden cambios a la estimación

--- construbot/proyectos/templatetags/projecttags.py
+++ construbot/proyectos/templatetags/projecttags.py
@@ -1,30 +1,38 @@
 import datetime
 from django import template
+from decimal import Decimal
 
 register = template.Library()
 
 @register.simple_tag
-def get_amortizacion_de_anticipo(porcentaje, monto_total):
-    return monto_total * (porcentaje / 100)
+def get_amortizacion_de_anticipo(porcentaje):
+    if porcentaje is not None:
+        return porcentaje * (porcentaje / 100)
+    else:
+        return Decimal(0)
 
 @register.simple_tag
 def get_subtotal(porcentaje, monto_total):
+    if not porcentaje:
+        porcentaje = Decimal(0)
+    if not monto_total:
+        monto_total = Decimal(0)
     return monto_total - (monto_total * (porcentaje / 100))
 
 @register.simple_tag
 def get_total_retenciones(retenciones, porcentaje, monto_total):
-	total_retenciones = 0
-	subtotal = get_subtotal(porcentaje, monto_total)
-	for ret in retenciones:
-		if ret.tipo == 'AMOUNT':
-			aux = ret.valor
-		else:
-			aux = subtotal * (ret.valor/100)
-		total_retenciones = total_retenciones+aux
-	return total_retenciones
+    total_retenciones = 0
+    subtotal = get_subtotal(porcentaje, monto_total)
+    for ret in retenciones:
+        if ret.tipo == 'AMOUNT':
+            aux = ret.valor
+        else:
+            aux = subtotal * (ret.valor/100)
+        total_retenciones = total_retenciones+aux
+    return total_retenciones
 
 @register.simple_tag
 def get_total_final(retenciones, porcentaje, monto_total):
-	subtotal = get_subtotal(porcentaje, monto_total)
-	total_retenciones = get_total_retenciones(retenciones, porcentaje, monto_total)
-	return subtotal - total_retenciones
+    subtotal = get_subtotal(porcentaje, monto_total)
+    total_retenciones = get_total_retenciones(retenciones, porcentaje, monto_total)
+    return subtotal - total_retenciones
