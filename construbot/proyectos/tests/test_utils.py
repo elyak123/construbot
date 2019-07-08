2ceffdeeea36e2d1dc911b3dd1979dc61712bcfa
Javier Llamas Ramirez <elyak.123@gmail.com>
2019-07-08 15:12:00 -0500

    correccion error en query para dashboard

---
+++ construbot/proyectos/tests/test_utils.py
@@ -0,0 +1,32 @@
+from unittest import mock
+from django.test import tag
+from test_plus.test import CBVTestCase
+from construbot.proyectos import utils
+
+
+class EstimacionesPendientesdePago(CBVTestCase):
+
+    @mock.patch('construbot.proyectos.utils.Estimate.objects.select_related')
+    @mock.patch('construbot.proyectos.utils.Estimate.objects.filter')
+    def test_estimaciones_pendients_pago_returns_qs_on_perms(self, mock_filter, mock_select_related):
+        mock_qs = mock.Mock()
+        mock_qs.filter = mock_filter
+        mock_select_related.return_value = mock_qs
+        mock_company = mock.Mock()
+        mock_user = mock.Mock()
+        utils.estimacionespendientes_pago(mock_company, True, mock_user)
+        mock_filter.assert_called_once_with(project__cliente__company=mock_company, invoiced=True, paid=False)
+
+
+class EstimacionesPendientesFacturacion(CBVTestCase):
+
+    @mock.patch('construbot.proyectos.utils.Estimate.objects.select_related')
+    @mock.patch('construbot.proyectos.utils.Estimate.objects.filter')
+    def test_estimaciones_pendients_facturacion_returns_qs_on_perms(self, mock_filter, mock_select_related):
+        mock_qs = mock.Mock()
+        mock_qs.filter = mock_filter
+        mock_select_related.return_value = mock_qs
+        mock_company = mock.Mock()
+        mock_user = mock.Mock()
+        utils.estimacionespendientes_facturacion(mock_company, True, mock_user)
+        mock_filter.assert_called_once_with(project__cliente__company=mock_company, invoiced=False)
