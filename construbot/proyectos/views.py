1624c88fc9915a35fc3c45c7c42e99475aa93fab
Javier Llamas Ramirez <elyak.123@gmail.com>
2019-07-24 10:25:47 -0500

    ya sube archivos grandes pero con diferente url

--- construbot/proyectos/views.py
+++ construbot/proyectos/views.py
@@ -2,7 +2,6 @@ import importlib
 import json
 from django.conf import settings
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
-from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse, reverse_lazy
 from django.db.models import Max, F, Q
 from django.db.models.functions import Lower
