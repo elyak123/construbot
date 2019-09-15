5c32b6a103f97c7f0904a3b546b05409942dff60
Javier Llamas Ramirez <elyak.123@gmail.com>
2019-09-15 16:43:53 -0500

    se quita la app wkhtmltopdf

--- construbot/config/settings/base.py
+++ construbot/config/settings/base.py
@@ -65,7 +65,6 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
-    'wkhtmltopdf',  # pdf generation
     'chunked_upload',  # user uploads
     'treebeard',
 ]
